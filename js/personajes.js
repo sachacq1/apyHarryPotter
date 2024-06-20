8542936c6493336053e9394d5c15a943b969d085
sachacq1 <sachacq1@gmail.com>
2024-06-20 19:51:29 -0300

    change pages

--- js/personajes.js
+++ js/personajes.js
@@ -2,7 +2,7 @@ const $card = document.getElementById("grid");
 const $form = document.getElementById("form");
 const $input = document.getElementById("input");
 let personajes = [];
-const imagenGenerica = '/assets/img/lenterayo.jpg'
+const imagenGenerica = '/assets/img/what.webp'
 
 fetch("https://hp-api.onrender.com/api/characters")
     .then((respuesta) => {
@@ -17,11 +17,17 @@ fetch("https://hp-api.onrender.com/api/characters")
 function mostrarPersonajes(personajes) {
     $card.innerHTML = "";
     personajes.forEach((personaje) => {
+        if (
+            !personaje.name ||
+            !personaje.actor
+        ) {
+            return;
+        }
         const imagen = personaje.image ? personaje.image : imagenGenerica;
         $card.innerHTML += `
             <div>
                 <h4>${personaje.name}</h4>
-                <img src="${personaje.image}" alt="imagen de ${personaje.name}">
+                <img src="${imagen}" alt="imagen de ${personaje.name}">
                 <p>Casa: ${personaje.house}</p>
                 <p>Fecha nac: ${personaje.dateOfBirth}</p>
                 <p>Patronus: ${personaje.patronus}</p>
